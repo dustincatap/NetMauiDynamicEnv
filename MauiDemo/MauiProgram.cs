e65730399dad525f1a43375d9b0b7ecb8b90528b
Dustin Catap <dustincatap@gmail.com>
2023-05-08 11:45:00 +0800

    Update

--- MauiDemo/MauiProgram.cs
+++ MauiDemo/MauiProgram.cs
@@ -1,24 +1,52 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace MauiDemo;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-#if DEBUG
-		builder.Logging.AddDebug();
+#if !RELEASE
+        builder.Logging.AddDebug();
+#endif
+        
+        // Select appsettings.json file based on build configuration
+#if DEV
+        var appSettingsFileName = "appsettings.dev.json";
+#elif PROD
+        var appSettingsFileName = "appsettings.prod.json";
 #endif
 
-		return builder.Build();
-	}
+        // Load appsettings.json file as part of configuration
+        var executingAssembly = Assembly.GetExecutingAssembly();
+        using (var jsonStream = executingAssembly.GetManifestResourceStream($"MauiDemo.{appSettingsFileName}"))
+        {
+            if (jsonStream == null)
+            {
+                throw new Exception($"Could not find {appSettingsFileName} file");
+            }
+
+            var config = new ConfigurationBuilder()
+                .AddJsonStream(jsonStream)
+                .Build();
+
+            var mySettings = config.Get<MySettings>();
+            Console.WriteLine($"AppEnv: " + mySettings?.AppEnv);
+
+            builder.Configuration.AddConfiguration(config);
+        }
+
+        return builder.Build();
+    }
 }
---
+++ MauiDemo/MySettings.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiDemo;
+
+public class MySettings
+{
+    public required string AppName { get; init; }
+
+    public required string AppEnv { get; init; }
+
+    public required string AppIdSuffix { get; init; }
+
+    public required string ServerUrl { get; init; }
+}
